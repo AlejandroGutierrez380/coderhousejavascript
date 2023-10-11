514cc87daf976431ba88393c270d3c89b8bd89c0
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2023-10-10 21:18:59 -0500

    Se agregaron arrays y dos tipos de math en el codigo

--- js/script.js
+++ js/script.js
@@ -2,59 +2,154 @@
 
 let colorProducto
 let tipoDeProducto
-let nombreUsuario
+
 let shippingCost = 0 // Inicializa el costo de envío en cero.
 
-nombreUsuario = prompt("¡Hola! Ingresa tu nombre")
+var nombreUsuario = prompt("¡Hola! Ingresa tu nombre")
+var edadUsuario = prompt("Por favor, ingresa tu edad")
+
+alert(
+  "¡Hola, " + nombreUsuario + "! Tienes " + edadUsuario + " años. Bienvenido."
+)
 
 function obtenerColorProducto() {
   while (true) {
-    const colorProducto = parseInt(
-      prompt(
-        "Ingrese el color del producto que desea:\n1-Negro\n2-Plateado\n3-Dorado"
-      )
+    const colorInput = prompt(
+      "Ingrese el color del producto que desea:\n1-Negro\n2-Plateado\n3-Dorado"
     )
+
+    const colorProducto = parseInt(colorInput)
+    let productoElegido = {}
 
     switch (colorProducto) {
       case 1:
+        productoElegido = {
+          nombre: "Negro",
+          precio: 100.567, // Precio con decimales
+          modelo: "Modelo Negro",
+          caracteristicas: ["Cromado", "Carbón"],
+        }
+        break
       case 2:
+        productoElegido = {
+          nombre: "Plateado",
+          precio: 120.789, // Precio con decimales
+          modelo: "Modelo Plateado",
+          caracteristicas: ["Cromado", "Plata"],
+        }
+        break
       case 3:
-        return colorProducto // Retorna el valor válido
+        productoElegido = {
+          nombre: "Dorado",
+          precio: 130.345, // Precio con decimales
+          modelo: "Modelo Dorado",
+          caracteristicas: ["Cromado", "Oro"],
+        }
+        break
       default:
         alert("No has introducido un valor válido.")
+        break
     }
+
+    // Redondear el precio a dos decimales
+    productoElegido.precio = Math.round(productoElegido.precio * 100) / 100
+
+    return productoElegido
   }
 }
 
 // Llama a la función para obtener el color del producto
 const colorElegido = obtenerColorProducto()
 
-// Ahora puedes utilizar 'colorElegido' en tu código principal
 console.log("El color elegido es:", colorElegido)
 
 // Función para obtener el tipo de producto
 function obtenerTipoDeProducto() {
+  var productosElegidos = [] // Array para almacenar los productos
+  var costoEnvio = 10
+  var totalAccesorios = 0 // Variable para rastrear el número total de accesorios seleccionados
+
   while (true) {
     const tipoDeProducto = parseInt(
       prompt("Ingrese el producto que desea:\n1-Aretes\n2-Anillos\n3-Pulseras")
     )
 
+    if (isNaN(tipoDeProducto) || tipoDeProducto < 1 || tipoDeProducto > 3) {
+      alert("No has introducido un valor válido.")
+      continue // Continuar el bucle para solicitar una entrada válida
+    }
+
+    let cantidad = 0
+    let precioUnitario = 0
+
     switch (tipoDeProducto) {
       case 1:
+        cantidad = parseInt(prompt("Ingrese la cantidad de aretes que desea:"))
+        precioUnitario = Math.abs(25) // Precio unitario con valor absoluto
+        productosElegidos.push({
+          tipo: "Aretes",
+          precioUnitario: precioUnitario,
+          material: "Acero Inoxidable",
+          cantidad: cantidad,
+          costoTotal: cantidad * precioUnitario,
+        })
+        totalAccesorios += cantidad // Sumar la cantidad de aretes seleccionados al total
+        break
       case 2:
+        cantidad = parseInt(prompt("Ingrese la cantidad de anillos que desea:"))
+        precioUnitario = Math.abs(30) // Precio unitario con valor absoluto
+        productosElegidos.push({
+          tipo: "Anillos",
+          precioUnitario: precioUnitario,
+          material: "Plata",
+          cantidad: cantidad,
+          costoTotal: cantidad * precioUnitario,
+        })
+        totalAccesorios += cantidad // Sumar la cantidad de anillos seleccionados al total
+        break
       case 3:
-        return tipoDeProducto // Retorna el valor válido
+        cantidad = parseInt(
+          prompt("Ingrese la cantidad de pulseras que desea:")
+        )
+        precioUnitario = Math.abs(40) // Precio unitario con valor absoluto
+        productosElegidos.push({
+          tipo: "Pulseras",
+          precioUnitario: precioUnitario,
+          material: "Cuero",
+          cantidad: cantidad,
+          costoTotal: cantidad * precioUnitario,
+        })
+        totalAccesorios += cantidad // Sumar la cantidad de pulseras seleccionadas al total
+        break
       default:
-        alert
         alert("No has introducido un valor válido.")
+        continue // Continuar el bucle para solicitar una entrada válida
     }
+
+    // Si el usuario selecciona un producto válido, salir del bucle.
+    break
+  }
+
+  // Calcular la suma total de los productos
+  var sumaTotal = productosElegidos.reduce(function (acumulador, producto) {
+    return acumulador + producto.costoTotal
+  }, 0)
+
+  // Agregar el número total de accesorios al costo total
+  sumaTotal += totalAccesorios
+
+  // Sumar el costo de envío a la suma total
+  sumaTotal += costoEnvio
+
+  return {
+    productos: productosElegidos,
+    costoEnvio: costoEnvio,
+    sumaTotal: sumaTotal,
   }
 }
-
 // Llamamos a la función para obtener el tipo de producto
 const tipoElegido = obtenerTipoDeProducto()
 
-// Luego puedes utilizar 'tipoElegido' en tu código principal
 console.log("El tipo de producto elegido es:", tipoElegido)
 
 const productWeights = [0.5, 0.8, 1.2] // Pesos de los productos (en kilogramos).
